7d57e0c139e76d2d55df8433c60b87bdfcd857b0
niamtokik <mk@steepath.eu>
2018-03-14 20:13:22 +0100

    create documentation reference, add unit test and migrate cut/copy function to stream_lib. Create stream_passive fsm.

--- src/stream_lib.erl
+++ src/stream_lib.erl
@@ -1,20 +1,77 @@
 %%%-------------------------------------------------------------------
 %%% @author Mathieu Kerjouan
 %%% @copyright (c) 2017, Mathieu Kerjouan
-%%% @doc documentation about this module
-%%%      ...
+%%% @doc = Standard Interface Definition and Messages =
+%%%
+%%%      == Input/Output Message ==
+%%%
+%%%      These messages are used to import/extract stream buffer.
+%%%
+%%%        - { input, bitstring() }
+%%%        - { output, pid(), bitstring() }
+%%%
+%%%      == Internal Message ==
+%%%
+%%%      Internal message used by our stream FSM.
+%%%
+%%%        - { Reference, insufficient, Replay}
+%%%
+%%%      == Buffer Interaction ==
+%%%
+%%%      Common interface to cut, copy, and map buffer. These
+%%%      function are usually used with our API.
+%%%
+%%%        - { cut, {Size, Shift, Options} }
+%%%        - { copy, {Size, Shift, Options} }
+%%%        - { map, {Size, Shift, Function, Options} }
+%%%
+%%%      == Automatic Action on Buffer ==
+%%%
+%%%      These messages are mainly used to act automatically
+%%%      on some patterns and alter our FSM state.
+%%%
+%%%        - { action, {Size, Pattern, Function, Options} }
+%%%        - { search, {Size, Pattern, Function, Options} }
+%%%        - { parse, {Module, Function, Args, Options} }
+%%%
+%%%      == Behavior Configuration ==
+%%%
+%%%      These messages are used to alter current FSM state
+%%%      variables.
+%%%
+%%%        - { set, mode, Size }
+%%%        - { set, limit, Size}
+%%%        - { set, marks }
+%%%
+%%%      == Spread (automatic and manual) ==
+%%%
+%%%      These messages will permit to make static or dynamic
+%%%      copy of the stream on other stream. The main stream
+%%%      is kept intact, but, based on some marks on it,
+%%%      forwarded to another copy of the FSM (with different
+%%%      behavior).
+%%%
+%%%        - wip.
+%%% 
+%%%      = stream_lib =
+%%%
+%%%      This file extract all internal functions from stream
+%%%      gen_statem. This make thing easier to test and standard
+%%%      to all other gen_stream implementation.
+%%%
 %%% @end
 %%%-------------------------------------------------------------------
 -module(stream_lib).
--export([input/2, input/3]).
--export([cut/3, cut/4]).
--export([copy/3, copy/4]).
--export([map/3, map/4]).
--export([filter/3, filter/4]).
--export([action/3, action/4]).
--export([search/3, search/4]).
--export([parse/3, parse/4]).
--export([continuator/3, continuator/4]).
+-export([input/2]).
+-export([cut/3]).
+-export([copy/3]).
+%-export([map/3, map/4]).
+%-export([filter/3, filter/4]).
+%-export([action/3, action/4]).
+%-export([search/3, search/4]).
+%-export([parse/3, parse/4]).
+%-export([continuator/3, continuator/4]).
+-compile(export_all).
 -include("stream.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
@@ -23,10 +80,14 @@
 %% @end
 %%--------------------------------------------------------------------
 -spec input( Data :: #state{}
-	   , Bitstring :: bitstring()
+	   , Input :: bitstring()
 	   ) -> {keep_state, #state{}}.		   
-input(Data, Bitstring) -> 
-    input(Data, Bitstring, []).
+input(#state{ buffer = Buffer } = Data, Input) ->
+    { keep_state
+    , Data#state{ buffer = <<Buffer/bitstring, Input/bitstring>> }
+    };
+input(_Data, Input) -> 
+    {stop, {badinput, Input}}.
 
 input_0000_test() ->
     IN = <<"thisisatest">>,
@@ -38,45 +99,52 @@ input_0001_test() ->
     OUT = {keep_state, #state{ buffer = <<"abcdthisisatest">> }},
     ?assertEqual(OUT, input(Data, IN)).
 
--spec input( Data :: #state{}
-	   , Bitstring :: bitstring()
-	   , Opts :: list()
-	   ) -> {keep_state, #state{}}.
-input(#state{ buffer = Buffer } = Data, Input, _Opts) ->
-    { keep_state
-    , Data#state{ buffer = <<Buffer/bitstring, Input/bitstring>> }
-    };
-input(Data, Bitstring, Opts) -> ok.
-
-input_1000_test() ->
-    ok.
-
 %%--------------------------------------------------------------------
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec cut( Data :: #state{}
-	 , From :: pid()
-	 , { Size :: non_neg_integer()
-	   , Shift :: non_neg_integer() }
-	 ) -> {keep_state, #state{}}.		  
-cut(Data, From, {Size, Shift}) -> 
-    ok.
+cut(Data, From, {Size, Shift}) ->
+    cut(Data, From, {Size, Shift, []});
+cut(#state{ mode = Mode, buffer = Buffer} = Data
+   ,From
+   ,{Size, Shift, Opts}) ->
+    ShiftSize = Shift*Mode,
+    PatternSize = Size*Mode,
+    try <<Head:ShiftSize/bitstring, 
+	  Pattern:PatternSize/bitstring, 
+	  Rest/bitstring>> = Buffer,
+	 { keep_state
+	 , Data#state{ buffer = <<Head/bitstring, Rest/bitstring>> }
+	 , [{reply, From, {Pattern, function}}] }
+    catch
+	error:{badmatch,_Reason} ->
+	    Insufficient = {From, {cut, Size, Shift, Opts} },
+	    { keep_state, Data#state{ insufficient = Insufficient } }
+    end.
 
 cut_0000_test() ->
-    ok.
-
--spec cut( Data :: #state{}
-	 , From :: pid()
-	 , { Size :: non_neg_integer()
-	   , Shift :: non_neg_integer() }
-	 , Opts :: term()
-	 ) -> {keep_state, #state{}}.
-cut(Data, From, {Size, Shift}, Opts) ->
-    ok.
-
-cut_1000_test() ->
-    ok.
+    Data = #state{ buffer = <<"abcdefghijkl">> },
+    IN = cut(Data, from, {8, 0}),
+    OUT = { keep_state
+	  , Data#state{ buffer = <<"bcdefghijkl">> }
+	  , [{reply, from, {<<"a">>, function}}] 
+	  },
+    ?assertEqual(OUT, IN).
+cut_0001_test() ->
+    Data = #state{ buffer = <<"a">> },
+    IN = cut(Data, from, {16,0}),
+    OUT = { keep_state
+	  , Data#state{ insufficient = {from, {cut, 16, 0, []}} }
+	  },
+    ?assertEqual(OUT, IN).
+cut_0002_test() ->
+    Data = #state{ buffer = <<"abc">> },
+    IN = cut(Data, from, {8, 8}),
+    OUT = { keep_state
+	  , Data#state{ buffer = <<"ac">> }
+	  , [{reply, from, {<<"b">>, function}}]
+	  },
+    ?assertEqual(OUT, IN).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -85,25 +153,48 @@ cut_1000_test() ->
 -spec copy( Data :: #state{}
 	  , From :: pid()
 	  , { Size :: non_neg_integer()
-	    , Shift :: non_neg_integer() }
+	    , Shift :: non_neg_integer()
+	    , Opts :: term() }
 	  ) -> {keep_state, #state{}}.
 copy(Data, From, {Size, Shift}) -> 
-    ok.
+    copy(Data, From, {Size, Shift, []});
+copy(#state{ mode = Mode, buffer = Buffer } = Data
+    ,From
+    ,{Size, Shift, Opts}) ->
+    ShiftSize = Shift*Mode,
+    PatternSize = Size*Mode,
+    try <<_:ShiftSize/bitstring, 
+	  Pattern:PatternSize/bitstring, 
+	  _/bitstring>> = Buffer,
+	 { keep_state, Data
+	 , [{reply, From, {Pattern, function}}] }
+    catch
+	error:{badmatch,_Reason} ->
+	    Insufficient = {From, {copy, Size, Shift, Opts} },
+	    { keep_state, Data#state{ insufficient = Insufficient } }
+    end.
 
 copy_0000_test() ->
-    ok.
-
--spec copy( Data :: #state{}
-	  , From :: pid()
-	  , { Size :: non_neg_integer()
-	    , Shift :: non_neg_integer() }
-	  , Opts :: term()
-	  ) -> {keep_state, #state{}}.
-copy(Data, From, {Size, Shift}, Opts) ->
-    ok.
-
-copy_1000_test() ->
-    ok.
+    Data = #state{ buffer = <<"abcdefghij">> },
+    IN = copy(Data, from, {8,0}),
+    OUT = { keep_state, Data
+	  , [{reply, from, {<<"a">>, function}}] 
+	  },
+    ?assertEqual(OUT, IN).
+copy_0001_test() ->
+    Data = #state{ buffer = <<"a">> },
+    IN = copy(Data, from, {16,0}),
+    OUT = { keep_state
+	  , Data#state{ insufficient = {from, {copy, 16, 0, []}} }
+	  },
+    ?assertEqual(OUT, IN).
+copy_0002_test() ->
+    Data = #state{ buffer = <<"abc">> },
+    IN = copy(Data, from, {8,8}),
+    OUT = { keep_state, Data
+	  , [{reply, from, {<<"b">>, function}}]
+	  },
+    ?assertEqual(OUT, IN).
 
 %%--------------------------------------------------------------------
 %% @doc
--- src/stream_passive.erl
+++ src/stream_passive.erl
@@ -1,16 +1,41 @@
 %%%-------------------------------------------------------------------
 %%% @author Mathieu Kerjouan
 %%% @copyright (c) 2017, Mathieu Kerjouan
-%%% @doc documentation about this module
-%%%      ...
+%%% @doc = Passive Stream FSM =
+%%%      
+%%%      This FSM manage only input stream and doesn't have to
+%%%      maintain state about external stream generator.
+%%%
 %%% @end
 %%%-------------------------------------------------------------------
 -module(stream_passive).
-% -behaviour(gen_statem).
+-behaviour(gen_statem).
 -compile([export_all]).
 
 %%--------------------------------------------------------------------
-%%
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+callback_mode() ->
+    state_functions.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+code_change(_,_,_,_) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+terminate(_,_,_) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
 %%--------------------------------------------------------------------
 passive(cast, Event, Data) ->
     handle_cast(Event, Data);
@@ -20,23 +45,54 @@ passive(info, Event, Data) ->
     handle_info(Event, Data).
 
 %%--------------------------------------------------------------------
-%%
+%% @doc
+%% @end
 %%--------------------------------------------------------------------
+handle_cast({input, Stream}, Data) ->
+    stream_lib:input(Data, Stream);
+
+handle_cast({action, {Size, Shift, Pattern, Function}}, Data) ->
+    stream_lib:action(Data, {Size, Shift, Pattern, Function});
+handle_cast({action, {Size, Shift, Pattern, Function, Opts}}, Data) ->
+    stream_lib:action(Data, {Size, Shift, Pattern, Function, Opts});
+
+handle_cast({search, {Pattern, Function}}, Data) ->
+    stream_lib:search(Data, {Pattern, Function});
+handle_cast({search, {Pattern, Function, Opts}}, Data) ->
+    stream_lib:search(Data, {Pattern, Function, Opts});
+
+handle_cast({parse, {Module, Function, Arguments}}, Data) ->
+    stream_lib:parse(Data, {Module, Function, Arguments});
+handle_cast({parse, {Module, Function, Arguments, Opts}}, Data) ->
+    stream_lib:parse(Data, {Module, Function, Arguments, Opts});
 handle_cast(_Event, Data) ->
     {keep_state, Data}.
 
 %%--------------------------------------------------------------------
-%%
+%% @doc
+%% @end
 %%--------------------------------------------------------------------
+handle_call(From, {cut, {Size, Shift}}, Data) ->
+    stream_lib:cut(Data, From, {Size, Shift});
+handle_call(From, {cut, {Size, Shift, Opts}}, Data) ->
+    stream_lib:cut(Data, From, {Size, Shift, Opts});
+
+handle_call(From, {copy, {Size, Shift}}, Data) ->
+    stream_lib:copy(Data, From, {Size, Shift});
+handle_call(From, {copy, {Size, Shift, Opts}}, Data) ->
+    stream_lib:copy(Data, From, {Size, Shift, Opts});
+
+handle_call(From, {map, {Size, Shift, Function}}, Data) ->
+    stream_lib:map(Data, From, {Size, Shift, Function});
+handle_call(From, {map, {Size, Shift, Function, Opts}}, Data) ->
+    stream_lib:map(Data, From, {Size, Shift, Function, Opts});
+
 handle_call(From, _Event, Data) ->
     {keep_state, Data, [{reply, From, ok}]}.
 
 %%--------------------------------------------------------------------
-%%
+%% @doc
+%% @end
 %%--------------------------------------------------------------------
 handle_info(_Event, Data) ->
     {keep_state, Data}.
-
-
-
-
